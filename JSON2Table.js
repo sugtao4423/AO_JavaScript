2c3211c91b8bf1225a5f86c8a42420b62b52bf3d
tao <sugtao4423@gmail.com>
2016-08-14 20:29:21 +0900

    引数をArrayに、undefinedの例外処理を追加

--- JSON2Table.js
+++ JSON2Table.js
@@ -1,31 +1,30 @@
-// targetElementId : insert table data element id
-// jsonKeys : json's key as array
-// jsonData : json text data
+var JSON2Table = function(options) {
+	var element, keys, json;
 
-var JSON2Table = function(targetElementId, jsonKeys, jsonData) {
-	// sample data
-	//var targetElementId = 'sample-table';
-	//var jsonData = '[{"date":"2016-08-11 19:00","temp":"20.96"},{"date":"2016-08-11 20:00","temp":"22.13"},{"date":"2016-08-11 21:00","temp":"21.89"},{"date":"2016-08-11 22:00","temp":"21.28"},{"date":"2016-08-11 23:00","temp":"20.81"},{"date":"2016-08-12 00:00","temp":"20.86"},{"date":"2016-08-12 01:00","temp":"22.15"},{"date":"2016-08-12 02:00","temp":"21.52"},{"date":"2016-08-12 03:00","temp":"20.61"},{"date":"2016-08-12 04:00","temp":"21.04"}]';
-	//var jsonKeys = ["date", "temp"];
-	// end sample
+	var optionNames = ['element', 'keys', 'json'];
 
-	// parse JSON text Data
-	var json = JSON.parse(jsonData);
+	for(var i = 0; i < optionNames.length; i++){
+		if(options[optionNames[i]] === undefined)
+			throw "undefined '" + optionNames[i] + "'";
+	}
+	element = options['element'];
+	keys = options['keys'];
+	json = JSON.parse(options['json']);
 
 	// create table element
 	var table = document.createElement("table");
 	// target element append table element
-	document.getElementById(targetElementId).appendChild(table);
+	document.getElementById(element).appendChild(table);
 
 	// create thead
 	var thead = table.createTHead();
 	// insert thead's row
 	var theadRow = thead.insertRow(-1);
-	// create th jsonKeys times, append thead's row, jsonKeys name title
-	for(var i = 0; i < jsonKeys.length; i++){
+	// create th keys times, append thead's row, keys name title
+	for(var i = 0; i < keys.length; i++){
 		var th = document.createElement("th");
 		theadRow.appendChild(th);
-		th.innerHTML = jsonKeys[i];
+		th.innerHTML = keys[i];
 	}
 
 	// create tbody
@@ -33,11 +32,11 @@ var JSON2Table = function(targetElementId, jsonKeys, jsonData) {
 	// insert tbody's row json times
 	for(var i = 0; i < json.length; i++){
 		var tbodyRow = tbody.insertRow(-1);
-		// create td jsonKeys times, append tbody's row, json value
-		for(var j = 0; j < jsonKeys.length; j++){
+		// create td keys times, append tbody's row, json value
+		for(var j = 0; j < keys.length; j++){
 			var td = document.createElement("td");
 			tbodyRow.appendChild(td);
-			td.innerHTML = json[i][jsonKeys[j]];
+			td.innerHTML = json[i][keys[j]];
 		}
 	}
 }
